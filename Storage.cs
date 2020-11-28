470f9cd04827cba7727c7c9da5c1ea79ddfa9601
Vladislav Kinyashov <indraayy322@gmail.com>
2020-11-28 13:14:15 +0500

    Update and rename Config.cs to Storage.cs

---
+++ Storage.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public static class Storage
+{
+	private static ScriptableObject[] _assets = null;
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+	private static void Load() =>
+		_assets = Resources.LoadAll<ScriptableObject>("");
+
+	public static T Get<T>() where T : ScriptableObject
+	{
+		for (int i = 0; i < _assets.Length; i++)
+			if (_assets[i] is T asset)
+				return asset;
+
+		return null;
+	}
+}
