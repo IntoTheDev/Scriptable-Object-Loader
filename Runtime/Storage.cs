0313da9bddbf155bf9727e50056530139a15b303
Vladislav Kinyashov <indraayy322@gmail.com>
2021-01-04 18:37:44 +0500

    Get method optimization

--- Runtime/Storage.cs
+++ Runtime/Storage.cs
@@ -22,7 +22,16 @@ namespace ToolBox.Loader
 
 		public static T Get<T>() where T : ScriptableObject, ILoadable
 		{
-			return GetAll<T>().FirstOrDefault();
+#if UNITY_EDITOR
+			if (!Application.isPlaying)
+				_assets = Resources.FindObjectsOfTypeAll<ScriptableObject>();
+#endif
+
+			for (int i = 0; i < _assets.Length; i++)
+				if (_assets[i] is T asset)
+					return asset;
+
+			return null;
 		}
 
 		public static IEnumerable<T> GetAll<T>() where T : ScriptableObject, ILoadable
